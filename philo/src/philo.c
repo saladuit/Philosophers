c9c4cfcae9fabfd0de62b67a0f6cc483e9f099b8
Saladin Afoh <43225170+saladuit@users.noreply.github.com>
2022-08-23 15:46:53 +0200

    made first thread

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,12 +6,21 @@
 /*   By: safoh <safoh@student.codam.nl>             //   \ \ __| | | \ \/ /   */
 /*                                                 (|     | )|_| |_| |>  <    */
 /*   Created: 2022/08/22 18:10:43 by safoh        /'\_   _/`\__|\__,_/_/\_\   */
-/*   Updated: 2022/08/23 15:09:55 by safoh        \___)=(___/                 */
+/*   Updated: 2022/08/23 15:45:32 by safoh        \___)=(___/                 */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
-
+void *work(void * p) {
+	if (p == NULL)
+		return (NULL);
+	return (NULL);
+}
 void	philo(void)
 {
+	pthread_t philosopher;
+
+	philosopher = NULL;
+	if (pthread_create(&philosopher, NULL, work, NULL))
+		printf("A thread error occured\n");
 }
